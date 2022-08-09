80cd10fd924a574c1c2b6ed013aa172f07b32b55
Aaron <aaron@Aarons-Air.lan>
2022-08-09 18:13:23 -0400

    added components and modules for styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
+import { MatCardModule } from '@angular/material/card'
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AllPetImagesComponent } from './components/allpets/allPetImages.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AllPetImagesComponent,
+    HeaderComponent,
+    SidenavComponent,
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatSidenavModule,
     AppRoutingModule,
     BrowserAnimationsModule
   ],
