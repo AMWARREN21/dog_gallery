836ba9ef2203205eee75dc29365ec96d33cdce5e
Aaron Warren <aaron.m.warren00@gmail.com>
2022-08-23 12:50:58 -0400

    initial commit

---
+++ src/app/components/dog-modal/dog-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dog-modal',
+  templateUrl: './dog-modal.component.html',
+  styleUrls: ['./dog-modal.component.css']
+})
+export class DogModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
