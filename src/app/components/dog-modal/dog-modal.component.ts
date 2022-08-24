82f245af84ef0e3136ef791986182435b575176b
Aaron Warren <aaron.m.warren00@gmail.com>
2022-08-24 13:24:53 -0400

    added modal component taking in the image interface

--- src/app/components/dog-modal/dog-modal.component.ts
+++ src/app/components/dog-modal/dog-modal.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { ModalImage } from '../../DogImage';
 
 @Component({
   selector: 'app-dog-modal',
@@ -7,7 +9,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DogModalComponent implements OnInit {
 
-  constructor() { }
+  constructor(@Inject(MAT_DIALOG_DATA) public data: ModalImage) {
+
+  }
 
   ngOnInit(): void {
   }
