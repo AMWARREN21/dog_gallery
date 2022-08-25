3ce3ca727e2208dad9deea9189a69df66ea67a79
Aaron Warren <aaron.m.warren00@gmail.com>
2022-08-25 17:36:19 -0400

    gets images when loaded and open modal function added

--- src/app/Pages/breed-page/breed-page.component.ts
+++ src/app/Pages/breed-page/breed-page.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { DogsService } from 'src/app/services/dogs.service';
+import { MatDialog } from '@angular/material/dialog';
+import { DogModalComponent } from 'src/app/components/dog-modal/dog-modal.component';
 
 @Component({
   selector: 'app-breed-page',
   templateUrl: './breed-page.component.html',
-  styleUrls: ['./breed-page.component.css']
+  styleUrls: ['../../components/allpets/allPetImages.component.css']
 })
 export class BreedPageComponent implements OnInit {
 
-  constructor() { }
+  breedImages: any = ''
+  modalImage: any = ''
 
-  ngOnInit(): void {
+  constructor(private route: ActivatedRoute, private DogsService: DogsService, private dialog: MatDialog) { }
+
+  openModal($event: Event) {
+    this.modalImage = ($event.target as HTMLImageElement).src
+    console.log(this.modalImage)
+    this.dialog.open(DogModalComponent, {
+      data: {
+        image: this.modalImage
+      }
+    })
   }
 
+  ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      let breed: any = params.get('breed')
+      this.DogsService.getBreedImages(breed).subscribe((image) => {
+        this.breedImages = image['message']
+      })
+    })
+  }
 }
--- src/app/Pages/sub-breed-page/sub-breed-page.component.ts
+++ src/app/Pages/sub-breed-page/sub-breed-page.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { DogsService } from 'src/app/services/dogs.service';
+import { MatDialog } from '@angular/material/dialog';
+import { DogModalComponent } from 'src/app/components/dog-modal/dog-modal.component';
 
 @Component({
   selector: 'app-sub-breed-page',
   templateUrl: './sub-breed-page.component.html',
-  styleUrls: ['./sub-breed-page.component.css']
+  styleUrls: ['../../components/allpets/allPetImages.component.css']
 })
 export class SubBreedPageComponent implements OnInit {
 
-  constructor() { }
+  subBreedImages: any = ''
+  modalImage: any = ''
 
-  ngOnInit(): void {
+  constructor(private route: ActivatedRoute, private DogsService: DogsService, private dialog: MatDialog) { }
+
+  openModal($event: Event) {
+    this.modalImage = ($event.target as HTMLImageElement).src
+    console.log(this.modalImage)
+    this.dialog.open(DogModalComponent, {
+      data: {
+        image: this.modalImage
+      }
+    })
   }
 
+  ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      let breed: any = params.get('breed')
+      let subBreed = params.get('subBreed')
+      this.DogsService.getSubreedImages(breed, subBreed).subscribe((image) => {
+        this.subBreedImages = image['message']
+      })
+    })
+  }
 }
