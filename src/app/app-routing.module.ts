7d4ca2d699887b00d5287da4aaa5764876cfee7c
Aaron Warren <aaron.m.warren00@gmail.com>
2022-08-25 17:31:08 -0400

    added routing for dog breeds and their sub breeds

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AllPetImagesComponent } from './components/allpets/allPetImages.component';
+import { BreedPageComponent } from './Pages/breed-page/breed-page.component';
+import { HomepageComponent } from './Pages/homepage/homepage.component';
+import { SubBreedPageComponent } from './Pages/sub-breed-page/sub-breed-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomepageComponent,
+    children: [
+      { path: '', component: AllPetImagesComponent },
+      { path: ':breed', component: BreedPageComponent },
+      { path: ':breed/:subBreed', component: SubBreedPageComponent }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
