9f5a2c635246d8f71da30391b94e72510e6f8371
Aaron Warren <aaron.m.warren00@gmail.com>
2022-08-25 14:47:35 -0400

    initial commit

---
+++ src/app/Pages/sub-breed-page/sub-breed-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sub-breed-page',
+  templateUrl: './sub-breed-page.component.html',
+  styleUrls: ['./sub-breed-page.component.css']
+})
+export class SubBreedPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
