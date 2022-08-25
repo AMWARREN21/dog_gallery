42b46a1014d1341d263d2c5887cb193c3675db66
Aaron Warren <aaron.m.warren00@gmail.com>
2022-08-25 17:36:43 -0400

    get sub breed and main breed images

--- src/app/services/dogs.service.ts
+++ src/app/services/dogs.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs'
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { DogImage } from '../DogImage';
 
 @Injectable({
@@ -18,5 +18,13 @@ export class DogsService {
 
   getAllBreeds(): Observable<DogImage> {
     return this.http.get<DogImage>(`${this.apiUrl}/breeds/list/all`)
+  }
+
+  getSubreedImages(breed: string | undefined, subBreed: string | null): Observable<DogImage> {
+    return this.http.get<DogImage>(`${this.apiUrl}/breed/${breed}/${subBreed}/images/random/20`)
+  }
+
+  getBreedImages(breed: string | undefined): Observable<DogImage> {
+    return this.http.get<DogImage>(`${this.apiUrl}/breed/${breed}/images/random/20`)
   }
 }
