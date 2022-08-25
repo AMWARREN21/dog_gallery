cb12a5703416aaa407adc2a09235622f26cafca0
Aaron Warren <aaron.m.warren00@gmail.com>
2022-08-25 17:35:06 -0400

    removed console.log

--- src/app/components/sidenav/sidenav.component.ts
+++ src/app/components/sidenav/sidenav.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { DogsService } from 'src/app/services/dogs.service';
-import { DogImage } from '../../DogImage';
 
 @Component({
   selector: 'app-sidenav',
@@ -17,7 +16,6 @@ export class SidenavComponent implements OnInit {
   ngOnInit(): void {
     this.DogsService.getAllBreeds().subscribe((dogs) => {
       (this.dogBreeds = dogs['message'])
-      console.log(this.dogBreeds)
     })
   }
 
