3e40ced3aaf2a91699c98eb8f805d21ad04bfeb6
Aaron Warren <aaron.m.warren00@gmail.com>
2022-08-30 22:08:35 -0400

    changed file structure

---
+++ dog-img-app/src/app/shared/components/dog-modal/dog-modal.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { ModalImage } from '../../../models/DogImage';
+
+@Component({
+  selector: 'app-dog-modal',
+  templateUrl: './dog-modal.component.html',
+  styleUrls: ['./dog-modal.component.css']
+})
+export class DogModalComponent implements OnInit {
+
+  constructor(@Inject(MAT_DIALOG_DATA) public data: ModalImage) {
+
+  }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ dog-img-app/src/app/shared/sidenav/sidenav.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { DogsService } from 'dog-img-app/src/app/services/dogs.service';
+import { DogBreedList } from '../../models/DogImage';
+
+@Component({
+  selector: 'app-sidenav',
+  templateUrl: './sidenav.component.html',
+  styleUrls: ['./sidenav.component.css']
+})
+export class SidenavComponent implements OnInit {
+
+  dogBreeds: DogBreedList = {
+    message: {},
+    status: ''
+  };
+
+  Object = Object
+
+  constructor(private DogsService: DogsService) { }
+
+  ngOnInit(): void {
+    this.DogsService.getAllBreeds().subscribe((dogs) => {
+      (this.dogBreeds.message = dogs['message'])
+    })
+  }
+
+}
