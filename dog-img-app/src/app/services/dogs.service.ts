b9bcb6c3cfbae2539fe7b112a75df8d88e8f99fb
Aaron Warren <aaron.m.warren00@gmail.com>
2022-08-30 22:05:54 -0400

    added open modal to pass into shared template html

--- dog-img-app/src/app/services/dogs.service.ts
+++ dog-img-app/src/app/services/dogs.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs'
 import { HttpClient } from '@angular/common/http';
-import { DogImage } from '../models/DogImage';
+import { MatDialog } from '@angular/material/dialog';
+import { DogImages } from '../models/DogImage';
+import { DogModalComponent } from '../shared/components/dog-modal/dog-modal.component';
 
 @Injectable({
   providedIn: 'root'
@@ -10,21 +12,29 @@ export class DogsService {
 
   private apiUrl = 'https://dog.ceo/api'
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private dialog: MatDialog) { }
 
-  getRandomImages(): Observable<DogImage> {
-    return this.http.get<DogImage>(`${this.apiUrl}/breeds/image/random/20`)
+  getRandomImages(): Observable<DogImages> {
+    return this.http.get<DogImages>(`${this.apiUrl}/breeds/image/random/20`)
   }
 
-  getAllBreeds(): Observable<DogImage> {
-    return this.http.get<DogImage>(`${this.apiUrl}/breeds/list/all`)
+  getAllBreeds(): Observable<DogImages> {
+    return this.http.get<DogImages>(`${this.apiUrl}/breeds/list/all`)
   }
 
-  getSubreedImages(breed: string | undefined, subBreed: string | null): Observable<DogImage> {
-    return this.http.get<DogImage>(`${this.apiUrl}/breed/${breed}/${subBreed}/images/random/20`)
+  getSubreedImages(breed: string | undefined, subBreed: string | null): Observable<DogImages> {
+    return this.http.get<DogImages>(`${this.apiUrl}/breed/${breed}/${subBreed}/images/random/20`)
   }
 
-  getBreedImages(breed: string | undefined): Observable<DogImage> {
-    return this.http.get<DogImage>(`${this.apiUrl}/breed/${breed}/images/random/20`)
+  getBreedImages(breed: string | undefined): Observable<DogImages> {
+    return this.http.get<DogImages>(`${this.apiUrl}/breed/${breed}/images/random/20`)
+  }
+
+  openModal($event: Event) {
+    this.dialog.open(DogModalComponent, {
+      data: {
+        image: ($event.target as HTMLImageElement).src
+      }
+    })
   }
 }
