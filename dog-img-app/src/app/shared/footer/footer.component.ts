a5ee8a5e19ee68d434dff773b648fba9d2f83358
Aaron Warren <aaron.m.warren00@gmail.com>
2022-08-31 21:26:30 -0400

    added event to change language

--- dog-img-app/src/app/shared/footer/footer.component.ts
+++ dog-img-app/src/app/shared/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-footer',
@@ -7,7 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+
+  constructor(private translate: TranslateService) { }
+
+  changeLang($event: Event) {
+    let lang: any = ($event.target as HTMLLIElement).textContent
+    this.translate.use(lang)
+  }
+
 
   ngOnInit(): void {
   }
