c21aeca533547b52bd65741c3006f0661c2a131a
Aaron Warren <aaron.m.warren00@gmail.com>
2022-09-16 14:41:57 -0400

    added filter functionality

--- src/app/shared/sidenav/sidenav.component.ts
+++ src/app/shared/sidenav/sidenav.component.ts
@@ -15,12 +15,44 @@ export class SidenavComponent implements OnInit {
     status: ''
   };
 
+  query: string = ''
+
+  original: DogBreedList = {
+    message: {},
+    status: ''
+  }
+
   constructor(private DogsService: DogsService) { }
 
+  // Object.keys(dogBreeds.message).filter(())
+
+  getResults(event: any) {
+    this.query = event
+    this.filter()
+  }
+
+  filter() {
+    let filterObj = this.original.message
+    this.dogBreeds.message = {}
+    for (let key of Object.keys(filterObj)) {
+      if (key.includes(this.query)) {
+        // console.log(key)
+        this.dogBreeds.message[key] = filterObj[key]
+      }
+    }
+
+    console.log(this.dogBreeds.message)
+  }
+
   ngOnInit(): void {
+
     //call service to getallbreeds and have the dogBreeds instance = result
     this.DogsService.getAllBreeds().subscribe((dogs: any) => {
-      (this.dogBreeds.message = dogs['message'])
+      // Object.keys(dogBreeds.message).filter(())
+      this.dogBreeds.message = dogs['message']
+      this.original.message = dogs['message']
+
+      this.filter()
     })
   }
 
