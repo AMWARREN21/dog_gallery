ba237b27ea6f9978ebd9500c9357c4c47ac89f4f
Aaron Warren <aaron.m.warren00@gmail.com>
2022-09-16 14:41:35 -0400

    added event to send input to sidenav

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-search-bar',
@@ -7,7 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchBarComponent implements OnInit {
 
+  @Output() filterParent: EventEmitter<any> = new EventEmitter()
+
   constructor() { }
+
+  public filterBreeds(event: Event) {
+    let inputValue = (event.target as HTMLInputElement).value
+    this.filterParent.emit(inputValue)
+  }
 
   ngOnInit(): void {
   }
